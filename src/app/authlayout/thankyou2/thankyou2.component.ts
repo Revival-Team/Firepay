1d930cd4804ad1a309a819a13345bd0a971a5295
Murad Gapurovich <darkkukri0550@gmail.com>
2022-09-18 14:25:24 +0300

    Подтверждение номера & Страница успешной авторизации

--- src/app/authlayout/thankyou2/thankyou2.component.ts
+++ src/app/authlayout/thankyou2/thankyou2.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Thankyou2Component implements OnInit {
 
-  constructor() { }
+  constructor() {
+    if (localStorage.getItem('auth_token') !== undefined && localStorage.getItem('auth_token') !== null) {
+      document.location.href = '/home';
+    }
+  }
 
   ngOnInit(): void {
   }
--- src/app/authlayout/verify/verify.component.ts
+++ src/app/authlayout/verify/verify.component.ts
@@ -11,25 +11,70 @@ export class VerifyComponent implements OnInit {
   code: string;
   error: string;
 
-  constructor(private api: Api) { }
+  constructor(private api: Api) {
+    if (localStorage.getItem('auth_token') !== undefined && localStorage.getItem('auth_token') !== null) {
+      document.location.href = '/home';
+    }
+  }
 
   ngOnInit(): void {
 
   }
 
   public sendCode(): void {
-    this.api.request('POST', 'users/confirm',
-      {'token': localStorage.getItem('guest_token'), 'phone': sessionStorage.getItem('phone'), 'code': this.code});
+    if (this.validate()) {
+      this.api.request('POST', 'users/confirm',
+        {'token': localStorage.getItem('guest_token'), 'phone': sessionStorage.getItem('phone'), 'code': this.code});
 
-    console.log({'token': localStorage.getItem('guest_token'), 'phone': sessionStorage.getItem('phone'), 'code': this.code});
+      console.log({'token': localStorage.getItem('guest_token'), 'phone': sessionStorage.getItem('phone'), 'code': this.code});
+
+      setTimeout(() => {
+        let response = JSON.parse(this.api.getResponse());
+        console.log(response);
+
+        if (response.status === 'error') {
+          /**
+           * Если код неверный
+           */
+          this.error = 'Введён неверный код.';
+        } else if (response.status === 'success') {
+          /**
+           * Если код верный
+           */
+          this.error = '';
+          console.log(sessionStorage.getItem('password'));
+
+          /**
+           * Регистрируем пользователя
+           */
+          this.proceedRegister(response.data.confirm_uuid);
+        }
+      }, 300);
+    } else {
+      this.error = 'Пожалуйста, заполните поля!';
+    }
+  }
+
+  proceedRegister(uuid: string): void {
+    this.api.request('POST', 'users/registration',
+      {'token': localStorage.getItem('guest_token'), 'phone': sessionStorage.getItem('phone'), 'name1': sessionStorage.getItem('name'),
+        'password': sessionStorage.getItem('password'), 'iscompany': false, 'confirm_uuid': uuid});
 
     setTimeout(() => {
       let response = JSON.parse(this.api.getResponse());
       console.log(response);
 
-      if (response.status === 'error') {
-        this.error = 'Введён неверный код.';
+      if (response.status === 'success') {
+        localStorage.setItem('uuid', response.data.confirm_uuid);
+        localStorage.setItem('uuid', response.data.confirm_uuid);
+        window.location.href = '/thankyou2';
+      } else {
+        this.error = 'Произошла ошибка. Пожалуйста, повторите попытку.';
       }
-    }, 300);
+    });
+  }
+
+  private validate(): boolean {
+    return !(this.code === '' || this.code === undefined);
   }
 }
